168b827f32087a04cb4b344d939d9e928a0903ee
Akram Khalid <akramkhalid12@gmail.com>
2020-02-07 12:24:13 -0600

    Updates

---
+++ precache-manifest.c26108c1654248ad3821d60588b630bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a32153d7d158d968af2df6dfc216b45",
+    "url": "/ar-episode1/index.html"
+  },
+  {
+    "revision": "5476251bb2178657d369",
+    "url": "/ar-episode1/static/css/main.a34e6493.chunk.css"
+  },
+  {
+    "revision": "c422d8f83a6a256ea89a",
+    "url": "/ar-episode1/static/js/2.7aadb8e4.chunk.js"
+  },
+  {
+    "revision": "3cd4b6bc4f51e64816d8a2b69bba1790",
+    "url": "/ar-episode1/static/js/2.7aadb8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5476251bb2178657d369",
+    "url": "/ar-episode1/static/js/main.f4c748c6.chunk.js"
+  },
+  {
+    "revision": "65d579cd12e215f1e471",
+    "url": "/ar-episode1/static/js/runtime-main.c85519c1.js"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/ar-episode1/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/ar-episode1/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/ar-episode1/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/ar-episode1/static/media/yourspace-min.5f9e3077.png"
+  }
+]);

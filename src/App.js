a601e424e5c9c9376c68ea07a3639fe1e3c26719
Akram Khalid <akramkhalid12@gmail.com>
2020-02-07 21:37:35 -0600

    Starter Files

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
-import React, { useEffect } from "react";
-import gsap from "gsap";
+import React from "react";
 import "./styles/App.scss";
-import Header from "./components/header";
-import Banner from "./components/banner";
-import Cases from "./components/cases";
-import IntroOverlay from "./components/introOverlay";
 
 function App() {
-  useEffect(() => {
-    //Grab inner height of window for mobile reasons when dealing with vh
-    let vh = window.innerHeight * 0.01;
-    //Set css varible vh
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-    // Preventing flash from happening.
-    gsap.to("body", 0, { css: { visibility: "visible" } });
-
-    //On load timeline
-    const tl = gsap.timeline();
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3
-      }
-    })
-      .to(".overlay-top", 1.6, {
-        height: 0,
-        ease: "expo.inOut",
-        stagger: {
-          amount: 0.4
-        }
-      })
-      .to(".overlay-bottom", 1.6, {
-        width: 0,
-        ease: "expo.inOut",
-        delay: -0.8,
-        stagger: {
-          amount: 0.4
-        }
-      })
-      .to(".intro-overlay", 0, { css: { display: "none" } })
-      .from(".case-image img", 1.6, {
-        scale: 1.4,
-        ease: "expo.inOut",
-        delay: -2,
-        stagger: {
-          amount: 0.4
-        }
-      });
-  }, []);
-
   return (
     <div className='App'>
-      <IntroOverlay />
-      <Header />
-      <Banner />
-      <Cases />
+      <h2>Hello this is your starter files </h2>
     </div>
   );
 }
